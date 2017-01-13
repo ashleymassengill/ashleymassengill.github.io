3c955033260bf0cf78388b724582c3974c59b362
Ashley Massengill <massengillashley@icloud.com>
2017-01-13 09:40:54 -0500

    Style update 1/12/17
    Sidebar update into list, top margin match sidebar margin, update
    resume file, sidebar background

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+// $(document).ready(function(event){
+//   // jQuery code
+// });
+
 $(document).ready(function(event){
-  // jQuery code
+    $('li').click(function(){
+      $('div').removeClass("active");
+      $(this).addClass("active");
+      console.log ("nav toggle working");
+  });
 });
