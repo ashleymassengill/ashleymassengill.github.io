8e60f77746fa1d38b49857196c4760cb7cb95a89
Ashley Massengill <massengillashley@icloud.com>
2017-01-30 11:10:56 -0500

    mobile nav 90%

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 //   // jQuery code
 // });
 
+// $(document).ready(function(event){
+//     $('li').click(function(){
+//       $('div').removeClass("active");
+//       $(this).addClass("active");
+//       console.log ("nav toggle working");
+//   });
+// });
+
 $(document).ready(function(event){
-    $('li').click(function(){
-      $('div').removeClass("active");
-      $(this).addClass("active");
-      console.log ("nav toggle working");
+  $(".fa-bars").click(function(){
+    $(".menunavMobile li").slideToggle("slow");
+    console.log ("nav toggle working");
   });
 });
