fb5058920a210816cf921b2393fde9a6fa59bffe
Ashley Massengill <massengillashley@icloud.com>
2020-07-07 18:18:55 -0400

    A lot more styling for desktop

--- js/script.js
+++ js/script.js
@@ -2,17 +2,24 @@
 //   // jQuery code
 // });
 
-// $(document).ready(function(event){
-//     $('li').click(function(){
-//       $('div').removeClass("active");
-//       $(this).addClass("active");
-//       console.log ("nav toggle working");
-//   });
-// });
-
 $(document).ready(function(event){
   $(".fa-bars").click(function(){
     $(".menunavMobile li").slideToggle("slow");
     console.log ("nav toggle working");
   });
 });
+
+//header sticky on scroll
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
